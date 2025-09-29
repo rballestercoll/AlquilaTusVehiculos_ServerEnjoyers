b289b3f13be7a26f44982d4b33315fde53df753f
Rafael Ballester Coll <rballestercoll@gmail.com>
2025-09-29 10:52:58 +0200

    Inicio Proyecto

---
+++ src/main/java/org/serverenjoyers/alquilatusvehiculos_serverenjoyers/AlquilaTusVehiculosServerEnjoyersApplication.java
@@ -0,0 +1,13 @@
+package org.serverenjoyers.alquilatusvehiculos_serverenjoyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaTusVehiculosServerEnjoyersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaTusVehiculosServerEnjoyersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serverenjoyers/alquilatusvehiculos_serverenjoyers/AlquilaTusVehiculosServerEnjoyersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serverenjoyers.alquilatusvehiculos_serverenjoyers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilaTusVehiculosServerEnjoyersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
